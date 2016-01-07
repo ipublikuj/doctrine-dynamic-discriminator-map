539afbb71b6256bca52ce3334d5c7102244f725a
Adam Kadlec <adam.kadlec@fastybird.com>
2016-01-07 21:53:17 +0100

    Updated travis condition

--- .travis.composer.php
+++ .travis.composer.php
@@ -16,8 +16,8 @@ $composer['require']['nette/application'] = $version;
 $composer['require']['nette/di'] = $version;
 
 if ($version === '~2.2.0') {
-	$composer['require']['kdyby/doctrine'] = '2.3.1';
-	$composer['require']['kdyby/events'] = '2.3.2';
+	$composer['require-dev']['kdyby/doctrine'] = '2.3.1';
+	$composer['require-dev']['kdyby/events'] = '2.3.2';
 }
 
 $content = json_encode($composer);
