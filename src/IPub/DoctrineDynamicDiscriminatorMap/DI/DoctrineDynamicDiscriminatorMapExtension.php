8799fe559d0e65399c4ab65ebdd4dd115f22856d
Adam Kadlec <adam.kadlec@fastybird.com>
2016-01-08 12:35:36 +0100

    For testing nette 2.2

--- src/IPub/DoctrineDynamicDiscriminatorMap/DI/DoctrineDynamicDiscriminatorMapExtension.php
+++ src/IPub/DoctrineDynamicDiscriminatorMap/DI/DoctrineDynamicDiscriminatorMapExtension.php
@@ -44,7 +44,7 @@ final class DoctrineDynamicDiscriminatorMapExtension extends DI\CompilerExtensio
 	{
 		$builder = $this->getContainerBuilder();
 
-		Nette\Diagnostics\Debugger::barDump($builder->getDefinitions());
+		Nette\Diagnostics\Debugger::dump($builder->getDefinitions());
 		$builder->getDefinition($builder->getByType('Doctrine\ORM\EntityManagerInterface'))
 			->addSetup('?->getEventManager()->addEventSubscriber(?)', ['@self', $builder->getDefinition($this->prefix('subscriber'))]);
 	}
